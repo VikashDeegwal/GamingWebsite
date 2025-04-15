250499c9d40d77f2dc61f436a917a8b6b9cfb493
Vikash <vikashkumardeegwal@gmail.com>
2025-04-15 10:43:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const nextbutten = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video');
+const  movielist =['video/hero-1.mp4','video/hero-2.mp4','video/hero-3.mp4','video/hero-4.mp4',];
+ 
+let index=0;
+nextbutten.addEventListener("click", function(){
+    index += 1
+    video.src =movielist[index];
+    if (index === 3){
+        index =-1;
+    }
+})
